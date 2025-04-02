995990e334ec68ca81ee670ea8cd657870a98a12
Tiffany Nebo <nebotiffany@gmail.com>
2025-04-02 14:35:13 +0100

    Added controller and route logic for authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,81 @@ const multer = require("multer");
 const uploadVideo = multer({
   dest: "/home/tiffany/repos/Ahny-Drive/public/data/videos",
 });
+require("dotenv").config();
+const User = require("../models/user");
+const { body, validationResult } = require("express-validator");
+//AUTHENTICATION----------------------------------------------------------------
 
+//VALIDATION LOGIC
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+const emailErr = "not a valid email format.";
+
+const validateUser = [
+  body("username")
+    .trim()
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Username ${lengthErr}`),
+  body("email").trim().isEmail().withMessage(`Email is ${emailErr}`),
+
+  body("password").isLength({ min: 5 }),
+  body("confirm-password")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Password mismatch"),
+];
+
+//-------------------------------------------------------------------------------------
+exports.getLoginPage = (req, res) => {
+  res.render("login");
+};
+
+exports.Logout = (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.send("You have been logged out");
+  });
+};
+
+exports.getSignupPage = (req, res) => {
+  res.render("signup");
+};
+
+exports.postSignupPage = [
+  validateUser,
+  async (req, res) => {
+    try {
+      //errors check
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).render("signup", {
+          errors: errors.array(),
+        });
+      }
+      //----------
+      const { firstName, lastName, username, email, password, adminCode } =
+        req.body;
+
+      const newUser = await User.create({
+        username: username,
+        email: email,
+        password: password,
+      });
+      res
+        .status(201)
+        .json({ message: `New user created succesfully`, user: newUser });
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: "Internal Server Error", err: err.message });
+    }
+  },
+];
+
+//------------------------------------------------------------------------------------
 exports.uploadVid = [
   uploadVideo.single("video"),
   (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,19 @@
 var express = require("express");
 var router = express.Router();
 const userController = require("../controllers/userController");
+const passport = require("passport");
 
-/* GET users listing. */
+router.get("/login", userController.getLoginPage);
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/users/signup",
+    successRedirect: "/",
+  })
+);
+router.get("/signup", userController.getSignupPage);
+router.post("/signup", userController.postSignupPage);
+
 router.get("/upload", userController.getUpload);
 router.post("/upload", userController.uploadVid);
 
