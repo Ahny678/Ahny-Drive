db6e0c117e53488c46d88e0babd904284c5ab740
Tiffany Nebo <nebotiffany@gmail.com>
2025-04-03 11:17:35 +0100

    indexcontroller for retriving all files without  a folder and folders updated the index UI for this functionality

---
+++ controllers/indexController.js
@@ -0,0 +1,45 @@
+const File = require("../models/file");
+const Folder = require("../models/folder");
+
+exports.Adrive = async (req, res) => {
+  const loneFiles = await getAllLoneFiles(req);
+  const folders = await getAllFolders(req);
+  const toFront = {
+    title: "Adrive",
+    userId: req.user.id,
+    loneFiles: loneFiles,
+    folders: folders,
+  };
+  res.render("index", toFront);
+};
+const getAllFolders = async (req, res) => {
+  try {
+    const folders = await Folder.findAll({ where: { userId: req.user.id } });
+    return folders;
+  } catch (err) {
+    throw new Error("Error retrieving folders: " + err.message);
+  }
+};
+
+const getAllLoneFiles = async (req) => {
+  try {
+    const files = await File.findAll({
+      where: { userId: req.user.id, folderId: null },
+    });
+    return files;
+  } catch (err) {
+    throw new Error("Error retrieving files: " + err.message);
+  }
+};
+
+exports.getAllFilesInFolder = (req, res) => {};
+
+exports.Auther = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res
+      .status(401)
+      .json({ message: "You are not authorized to view this page" });
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var router = express.Router();
+const indexController = require("../controllers/indexController");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express", userId: req.user.id });
-});
+router.get("/", indexController.Auther, indexController.Adrive);
 
 module.exports = router;
